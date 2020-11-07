f40de8bdada427eeecd3bba4cf6565f195185a68
Akeeb Ismail <ismailakeeb@gmail.com>
2020-11-07 17:59:09 +0100

    not found routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,11 @@ const express = require('express')
 const router = express.Router()
 const userRoutes = require('./user.route')
 router.use('/users', userRoutes)
-
+router.use((req, res, next) => {
+    return res.status(404).send({
+        status: 404,
+        message: 'Requested endpoint is not found.',
+        data: []
+    })
+})
 module.exports = router;
