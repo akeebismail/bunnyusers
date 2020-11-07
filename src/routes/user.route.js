6600b9fc80d5f889aa68da5c2e34af4bb0a6b1fa
Akeeb Ismail <ismailakeeb@gmail.com>
2020-11-07 18:29:08 +0100

    crud users completed

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,10 +2,30 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user.model')
 const validate = require('../middlewares/validate')
-const {createUserValidation} = require('../validations/user.validation')
+const {createUserValidation, updateUserValidation} = require('../validations/user.validation')
 router.post('/create', validate(createUserValidation),async (req, res) => {
     const response = await User.createUser(req.body)
     return res.send(response)
 })
+router.put('/:id/update', validate(updateUserValidation), async (req, res) => {
+    const response = await User.findOneAndUpdate({_id: req.params.id}, {
+        name: req.body.name,
+        email: req.body.email
+    })
+    return res.send(response);
+})
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).lean().exec()
+    return res.send(user)
+})
 
+router.delete('/:id', async (req, res) => {
+    const user = await User.findOneAndDelete({_id: req.params.id}).exec()
+    return res.send(user)
+})
+
+router.get('/', async (req, res) => {
+    const users = await User.find().exec();
+    return res.send(users);
+})
 module.exports = router;
--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -3,5 +3,9 @@ const Joi = require('joi')
 module.exports = {
     createUserValidation: Joi.object({
         name: Joi.string().required()
+    }),
+    updateUserValidation: Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().email().required()
     })
 }
